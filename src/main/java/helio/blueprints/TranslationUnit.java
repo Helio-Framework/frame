7fde4feedddc93e95fc22ab7ccd64ec973b4a6b2
Andrea Cimmino Arriaga <cimmino@fi.upm.es>
2022-04-17 16:23:17 +0200

    v0.4.14

--- src/main/java/helio/blueprints/TranslationUnit.java
+++ src/main/java/helio/blueprints/TranslationUnit.java
@@ -11,18 +11,7 @@ import java.util.concurrent.Callable;
 
 public interface TranslationUnit extends ConfigurableBlueprint {
 	
-	/**
-	 * This method retrieves the time that a {@link TranslationUnit} is run periodically. Uniquely {@link TranslationUnit} of {@link UnitType} Scheduled report a non null value. 
-	 * @return the scheduled time in milliseconds or null if the {@link TranslationUnit} is not {@link UnitType} Scheduled
-	 */
-	public Integer getScheduledTime();
 	
-	/**
-	 * This method sets the time that a {@link TranslationUnit} is run periodically. Setting this value make only sense if the {@link TranslationUnit} is {@link UnitType} Scheduled.
-	 * @param ms the new time in milliseconds to run periodically this {@link TranslationUnit} 
-	 */
-	public void setScheduledTime(Integer ms);
-		
 	/**
 	 * This method returns the {@link UnitType} of a {@link TranslationUnit}  
 	 * @return one of Scheduled, Sync, or Async
